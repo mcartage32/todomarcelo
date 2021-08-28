5bdebbfc76ace1a58105c2c8e4b468ce473fd32b
mcartage32 <mcartage33@gmail.com>
2021-08-28 14:11:04 -0500

    Add entity

---
+++ TodoMarcelo.Common/Responses/Responses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoMarcelo.Common.Responses
+{
+    public class Responses
+    {
+        public bool IsSucess { get; set; }
+
+        public string Message {get; set; }
+
+        public object Result { get; set; }
+    }
+}
---
+++ TodoMarcelo.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoMarcelo.Functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+
+    }
+}
